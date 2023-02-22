794bc46c29d4ec89ca363efff681a569a5e37604
Bikash Poudel <31735302+bikashpoudel4@users.noreply.github.com>
2023-02-22 11:09:15 +0100

    str

---
+++ 5-Strings/1-Strings.py
@@ -0,0 +1,13 @@
+"""STRINGS"""
+
+"""
+- In Python, Just as in most of the programming languages you must put any free text inside a pair of quote characters.
+- Otherwise Python will try to find the meaning in the text.1
+- The pieces of texts are called "Strings".
+- You can put string between two single quotes ie: ' ' or between two double quotes: " " , which does not matter 
+"""
+
+momo = "Buff momo with jhol."
+drink = 'cola'
+print(momo)
+print(drink)
---
+++ 5-Strings/2-LongLines.py
@@ -0,0 +1,15 @@
+"""Long Lines"""
+
+text = "xyz" "aec"
+print(text)
+
+other = "asdfghj"
+print(other)
+
+long_string = "One" "Two" "Three"
+print(long_string)
+
+short_rows = "one" \
+            "two" \
+            "three"
+print(short_rows)
